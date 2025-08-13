f361a476dad6d8b04f4bad393fa6911fb3f2cd94
Erick Diego <diegoerick2006@gmail.com>
2025-08-13 16:31:11 -0300

    learn(ap01-js) encerra o codigo 1.6.1
    - lidamos com vetores
    - lidamos com funções

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,6 @@
 //declaração de variaveis
 //const, let e var
+//eslint
 
 //===============================================
 
@@ -53,20 +54,68 @@
 
 //===============================================
 
-const n1 = 2
-const n2 = '2'
-const n3 = n1 + n2
-console.log(n3)
-const n4 = n1 + Number(n2)
-console.log(n4)
+// const n1 = 2
+// const n2 = '2'
+// const n3 = n1 + n2
+// console.log(n3)
+// const n4 = n1 + Number(n2)
+// console.log(n4)
 
-//===============================================
+// //===============================================
 
-console.log(1 == 1) // true
-console.log(1 == '1')// true
-console.log(1 === '1')// false
-console.log(1 ==[1])//true
-console.log(1 == [1, 1])//false
-console.log(null == undefined)//true
-console.log([] == false)//true
-console.log([] = [])//false
+// console.log(1 == 1) // true
+// console.log(1 == '1')// true
+// console.log(1 === '1')// false
+// console.log(1 ==[1])//true
+// console.log(1 == [1, 1])//false
+// console.log(null == undefined)//true
+// console.log([] == false)//true
+// console.log([] = [])//false
+// // ===============================================
+// const triplo = function(n = 5){
+//     return 3 * n
+// }
+// console.log(triplo())
+// console.log(triplo(10))
+// // arrow functions
+// const hello = () =>{
+//     console.log("Hello")
+// }
+// // ativação
+// hello()
+// // tem exatamente um parâmetro: parenteses podem ser omitidos
+// const dobro = n => 2 * n
+// console.log(dobro(2))
+// // tem somente umua linha as chaves podem ser omitidas
+// const hello = () => console.log("Hello")
+// // tem somente uma linha que produz um valor a ser devolvido
+// const dobro = (n) => {
+//     console.log('Calculando o dobro...')
+//     return 2 * n
+// }
+// console.log(dobro(1))
+// const dobro => 2 * n
+// console.log(dobro(5))
+
+// vetores, parte 2
+// const nomes = ['Ana Maria', 'Antonio', 'Rodrigo', 'Alex', 'Cristina']
+// A função filter percorre uma coleção e retorna apenas os elementos 
+// que satisfazem uma condição especificada por uma função.
+// const apenasComA = nomes.filter(nome => nome.startsWith('A'))
+// console.log(apenasComA)
+
+// const res = nomes.map(function(nome){return nome[0]})
+// console.log(res)
+
+// Dada a coleção a seguir, produzir outre coleção contendo o quadrado de cada número
+// usando arrow functions e digitando a menor quantidade de caracteres possivel
+
+// const numeros = [1, 2, 3] //[1, 4, 9]
+// console.log(numeros.map(n => n * n))
+
+// const todosComeçamComA =nomes.every(n => n.startsWith('A'))
+// const aoMenosUmComeçaComA =nomes.some(n => n.startsWith('A'))
+
+const valores = [1, 2, 3, 4]
+// const soma = valores.reduce((acm, v) => acm + v)
+// console.log(soma)
